74038376a3686482f73c51f33d3a41d6e2c35e88
Tamaki Awana <69060391+t-awana@users.noreply.github.com>
2020-10-20 06:20:57 +0900

    Released

--- MSP_AllOffPatchMZ.js
+++ MSP_AllOffPatchMZ.js
@@ -7,7 +7,7 @@
  * @plugindesc MultiSoundPlayerで設定したサウンドを終了時などに一括で解除します。
  * @author 沫那環(Tamaki Awana)
  *
- * @help n2naokun(柊菜緒)さん作、リクドウさん移植の、
+ * @help n2naokun(柊菜緒)さんMV版作、リクドウさん移植の、
  * RPGツクールMZプラグイン素材「MultiSoundPlayer」に、
  * タイトル画面移行時やセーブデータのロード時の暗転に
  * 設定していたサウンドを一括解除する機能を追加します。
@@ -32,12 +32,13 @@
         Scene_Base.prototype.fadeOutAll = function() {
             //ExSoundに収めている識別子全ての名前を、配列としてExSList作って突っ込む
             let ExSList = Object.keys(ExSound);
+            //識別子の個数もExSLenに突っ込む
             let ExSLen = ExSList.length
             //識別子の個数もExSLenに突っ込む
             let i = 0;
             while (ExSLen > 0){
-            //ExSoundに収めている識別子の数だけUtility.delSoundと同じ処理を繰り返す
-            //頭から削ってを繰り返すだけの頭の悪い処理なんで、誰か上手に書き直して欲しい……
+            //ExSoundに収めている配列に対応する識別子を、頭から削って、
+            //配列の個数を取得するを繰り返すという頭の悪い処理なんで、誰か上手に書き直して欲しい……
                 if (ExSoundBuffer[String(ExSList[0])]) {
                     ExSoundBuffer[String(ExSList[0])].stop();
                     ExSoundBuffer[String(ExSList[0])] = null;
@@ -46,12 +47,13 @@
                     delete ExSound[String(ExSList[0])];
                     ExSoundType[String(ExSList[0])] = null;
                     delete ExSoundType[String(ExSList[0])];
+                    //ExSListに収められた配列は頭から消していく
                     ExSList.shift();
+                    //配列の個数を再取得
                     ExSLen = ExSList.length
                 };
             };
         //fadeOutAllの元の処理を呼び出す
         _Scene_Base_fadeOutAll.call(this)
         };
-
 })();
