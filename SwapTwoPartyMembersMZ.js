bdc489ffcc52bad7131a70664503b90b1c1e62fe
Tamaki Awana <69060391+t-awana@users.noreply.github.com>
2021-07-12 04:24:41 +0900

    英語表示に対応

--- SwapTwoPartyMembersMZ.js
+++ SwapTwoPartyMembersMZ.js
@@ -1,19 +1,43 @@
 //=============================================================================
 // SwapTwoPartyMembersMZ.js
 //=============================================================================
-
+/*:
+ * @target MZ
+ * @plugindesc At a two-person party, if you select "Formation", the two will be replaced immediately.
+ * @author Tamaki Awana(Original:Toru Higuruma)
+ *
+ * @help At a two-person party, if you select "Formation" on the menu scene,
+ * the order of the two will be changed immediately.
+ * 
+ * Plugin Commands:
+ * This plugin does not provide plugin commands.
+ * 
+ * Update History:
+ * ver.1.0.1 English supported.
+ * ver.1.0 Release
+ * 
+ * ---
+ * 
+ * This plugin is released under MIT license.
+ * https://opensource.org/licenses/mit-license.php
+ * 
+ * This plugin is based on a Toru Higuruma's RGSS3 material
+ * "Skip actor select in menu"(メニューのアクター選択省略).
+ * Thanks to Toru Higuruma (https://github.com/neofuji).
+*/
 /*:ja
  * @target MZ
  * @plugindesc 2人パーティーの時、並び替えを選ぶと直ちに2人を入れ替えるようにします。
- * @author 沫那環(Tamaki Awana)
+ * @author 沫那環（原案：Toru Higuruma）
  *
- * @help　2人パーティーの時、メニュー画面で並び替えを選ぶと、
+ * @help 2人パーティーの時、メニュー画面で並び替えを選ぶと、
  * 直ちに2人の順番を入れ替えるようにします。
  * 
  * 【プラグインコマンドについて】
  * このプラグインに、プラグインコマンドはありません。
  * 
  * 【更新履歴】
+ * ver.1.0.1 英語表示に対応
  * ver.1.0 公開
  * 
  * ---
