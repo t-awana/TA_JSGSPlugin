6f7a503866442c27f963d341d962c8b9e148cd62
Tamaki Awana <69060391+t-awana@users.noreply.github.com>
2022-03-30 06:54:46 +0900

    英語版ヘルプの誤字を修正。

--- TA_AreaElementsMZ.js
+++ TA_AreaElementsMZ.js
@@ -54,6 +54,13 @@
  *  it activates the area elements offset effect
  *  by the area elements of the versus elements.
  * 
+ * ･<AE_DontAdjust>
+ *  When this note tag is in the note,
+ *  the operation to add or delete area elements specified
+ *  in the equipment, state, actor, and class will not be performed.
+ *  By setting this tag, you can create states and skills
+ *  that do not cause additional area element fluctuations.
+ * 
  *
  * You can enter the these note tag in the note of skill / item.
  * ･<AE_Cost:[element id]>
@@ -79,13 +86,6 @@
  * ･<AE_Need_Nil>
  *  The condition for using skills and items is that there are no area elements.
  *
- * ･<AE_DontAdjust>
- *  When this note tag is in the note,
- *  the operation to add or delete area elements specified
- *  in the equipment, state, actor, and class will not be performed.
- *  By setting this tag, you can create states and skills
- *  that do not cause additional area element fluctuations.
- * 
  *
  * Plugin Commands:
  *  Add Stable Area Element [element id]
@@ -128,6 +128,7 @@
  * 
  * 
  * Update History:
+ * ver.1.0.1 Fixed a typo in the English help.
  * ver.1.0   Released.
  * 
  * ---
@@ -519,6 +520,7 @@
  * 
  * 
  * 【更新履歴】
+ * 　ver.1.0.1 英語版ヘルプの誤字を修正。
  * 　ver.1.0   公開
  * 
  * ---
